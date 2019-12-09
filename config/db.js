e0c0525d3343c6c00c3b564e8aeea65ffd45ed2a
david.tran <david.tran@terralogic.com>
2019-12-09 15:32:30 +0700

    add route and connect db

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const urlDb = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(urlDb, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Db connected...')
+    } catch(err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
