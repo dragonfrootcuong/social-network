d4afad21abe626b140d052e7e297089677c17a6f
david.tran <david.tran@terralogic.com>
2019-12-18 16:26:36 +0700

    build front-end

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,45 @@
+import React, {useState, Fragment} from 'react'
+import {Link} from 'react-router-dom';
+
+const Login = () => {
+    const [formData, setFormData] = useState(
+        {email: '', password: '',}
+    );
+    
+    const {email, password} = formData;
+    const onChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value })
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+    return (
+        <Fragment>
+        <section className="container">
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead"><i className="fas fa-user"></i> Sign In To Your Account</p>
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" required/>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password} onChange={e => onChange(e)}
+              minLength="6"
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </section>
+      </Fragment>
+    )
+}
+
+export default Login;
